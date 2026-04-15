f14444f035a397fdfa77954564472401a550eb9c
aaa234a <234104825+aaa234a@users.noreply.github.com>
2026-04-15 11:04:04 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,22 @@
+import subprocess
 from flask import Flask, send_from_directory
-import os
 
 app = Flask(__name__, static_folder="noVNC")
 
-# ルートはvnc.html
 @app.route("/")
 def index():
     return send_from_directory("noVNC", "vnc.html")
 
-# 🔥 重要：ディレクトリ丸ごと配信
 @app.route("/<path:path>")
 def static_files(path):
-    file_path = os.path.join("noVNC", path)
-
-    # 存在チェック（デバッグ用）
-    if not os.path.exists(file_path):
-        return f"404 NOT FOUND: {path}", 404
-
     return send_from_directory("noVNC", path)
+
+# 🔥 websockify起動
+subprocess.Popen([
+    "websockify",
+    "6080",
+    "host.docker.internal:5900"
+])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
